283c2fe60ef8032c0142b7f4acb4bf10c53bfd97
James Socol <me@jamessocol.com>
2026-01-06 12:14:50 -0500

    use the tools that already exist

--- main.go
+++ main.go
@@ -9,12 +9,12 @@ import (
 	"sync"
 	"time"
 
+	"github.com/spiffe/go-spiffe/v2/spiffetls/tlsconfig"
 	"github.com/spiffe/go-spiffe/v2/workloadapi"
 	"jsocol.io/middleware/logging"
 
 	"jsocol.io/spiffe-authz-proxy/authorizer"
 	"jsocol.io/spiffe-authz-proxy/handlers"
-	"jsocol.io/spiffe-authz-proxy/tlsconfig"
 	"jsocol.io/spiffe-authz-proxy/upstream"
 )
 
@@ -61,21 +61,16 @@ func main() {
 		handlers.WithAuthorizer(authz),
 	)
 
-	cfger, err := tlsconfig.New(
-		tlsconfig.WithLogger(logger),
-		tlsconfig.WithSource(x509source),
-	)
-	if err != nil {
-		logger.Error("could not create tls config", "error", err)
-		os.Exit(3)
-	}
+	// TODO: tlsconfig.AuthorizeMemberOf(td)
+	tdAuthorizer := tlsconfig.AuthorizeAny()
+	tlsConfig := tlsconfig.MTLSServerConfig(x509source, x509source, tdAuthorizer)
 
 	bindAddr := envDefault("BIND_ADDR", ":8443")
 	srv := &http.Server{
 		Addr:                         bindAddr,
 		DisableGeneralOptionsHandler: true,
 		Handler:                      logging.Wrap(proxy, logging.WithLogger(logger)),
-		TLSConfig:                    cfger.GetConfig(),
+		TLSConfig:                    tlsConfig,
 	}
 
 	startupCancel()
