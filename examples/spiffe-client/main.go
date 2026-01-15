994756e633d864af0d33b945c1f6f22c83c1593a
James Socol <me@jamessocol.com>
2026-01-14 22:28:34 -0500

    start an examples directory

---
+++ examples/spiffe-client/main.go
@@ -0,0 +1,32 @@
+package main
+
+import (
+	"context"
+	"fmt"
+	"net/http"
+	"os"
+
+	"github.com/spiffe/go-spiffe/v2/spiffeid"
+	"github.com/spiffe/go-spiffe/v2/spiffetls/tlsconfig"
+	"github.com/spiffe/go-spiffe/v2/workloadapi"
+)
+
+func main() {
+	ctx := context.Background()
+	x509s, _ := workloadapi.NewX509Source(ctx)
+	authz := tlsconfig.AuthorizeMemberOf(spiffeid.RequireTrustDomainFromString("spiffe://example.org"))
+	client := &http.Client{
+		Transport: &http.Transport{
+			TLSClientConfig: tlsconfig.MTLSClientConfig(x509s, x509s, authz),
+		},
+	}
+
+	addr := os.Getenv("SERVER_ADDR")
+
+	resp, err := client.Get(fmt.Sprintf("https://%s/hi", addr))
+	if err != nil {
+		panic(err)
+	}
+
+	fmt.Printf("got response: %v\n", resp)
+}
